026f1affa86b5bdf472ab0622c1a1e6ec323e7cf
valumin <mohdbilto@gmail.com>
2023-07-15 18:36:50 +0300

    - Initial commit

---
+++ js/main.js
@@ -0,0 +1,40 @@
+$(document).ready(() => {
+    
+    adjustToScreenSize()
+    
+})
+
+$(window).on('resize', () => {
+    adjustToScreenSize()
+})
+
+
+function adjustToScreenSize(){
+
+    // Individual page height
+    var clientHeight = $(window).height()
+    var clientWidth = $(window).outerWidth()
+    $('.page').css('min-height', clientHeight)
+
+    // center-title-widget vertical center align
+    var widgetHeight = $('.centered-title-widget').height()
+    var _topMargin = clientHeight / 2 - widgetHeight / 1.5
+    $('.centered-title-widget').css('margin-top', _topMargin)
+
+    // Level meter vertical center align
+    var meterHeight = $('#level-meter').height()
+    var _meterOffset = (clientHeight - meterHeight) / 2
+    $('#level-meter').css('top', _meterOffset)
+    
+    meterEvenSpace()
+}
+
+function meterEvenSpace() {
+    var meterHeight = $('#level-meter').height()
+    var spacing = meterHeight / 10;
+    $(".meter-indicator").css('top', function() {
+        return $(this).index() * spacing
+      })
+
+}
+
